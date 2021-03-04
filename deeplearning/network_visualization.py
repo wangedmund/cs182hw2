da84cf05af6ab2a2821d32613029eb5ca38a0cb6
Edmund Wang <wangedmund@berkeley.edu>
2021-03-03 21:42:36 -0800

    fixing class visualization error

--- deeplearning/network_visualization.py
+++ deeplearning/network_visualization.py
@@ -104,7 +104,7 @@ def update_class_visulization(model, target_y, l2_reg, learning_rate, img):
     # Be very careful about the signs of elements in your code.            #
     ########################################################################
     score = model(img)[:,target_y] - l2_reg*(torch.square(torch.linalg.norm(img)))
-    dX = torch.autograd.grad(score, img, torch.ones(score.shape))[0] - 2*l2_reg*img
+    dX = torch.autograd.grad(score, img, torch.ones(score.shape))[0] + 2*l2_reg*img
     # img.data = torch.add(img.data, dX, alpha = learning_rate)
     img.data += learning_rate * dX
     ########################################################################
